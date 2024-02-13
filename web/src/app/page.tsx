b72f1ef48eb677ed1ccbf6cdff01badf82645cd9
Bruno <skyheads@live.com>
2024-02-13 04:07:19 -0300

    feat: hero section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { User } from 'lucide-react'
-
+import Image from 'next/image'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
@@ -7,10 +8,8 @@ export default function Home() {
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         {/* Blur */}
         <div className=" absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
-
         {/* Stripes */}
-        <div className="bg-stripes absolute bottom-0 right-2 top-0 w-2" />
-
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
         {/* Sign In */}
         <a
           href=""
@@ -24,6 +23,25 @@ export default function Home() {
             memórias
           </p>
         </a>
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+          <div className="max-w-[450px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
       </div>
       {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
