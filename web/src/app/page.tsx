5afad3eea7ec89a743c5b8a5f8797b498c95956c
Bruno <skyheads@live.com>
2024-02-14 03:46:50 -0300

    refactor: create some components for homepage

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,8 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { SignIn } from '@/components/SignIn'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
@@ -11,61 +13,15 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
         {/* Sign In */}
-        <a
-          href={`https://www.github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias
-          </p>
-        </a>
+        <SignIn />
         {/* Hero */}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="NLW Spacetime" />
-          <div className="max-w-[450px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase text-black transition-colors hover:bg-green-700"
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
+        <Hero />
         {/* Copyright */}
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com{' '}
-          <a
-            href="https://nextjs.org/"
-            target="_blank"
-            rel="noreferrer"
-            className="underline transition-colors hover:text-gray-100"
-          >
-            NEXTJS 14
-          </a>
-        </div>
+        <Copyright />
       </div>
       {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com{' '}
+      <a
+        href="https://nextjs.org/"
+        target="_blank"
+        rel="noreferrer"
+        className="underline transition-colors hover:text-gray-100"
+      >
+        NEXTJS 14
+      </a>
+    </div>
+  )
+}
