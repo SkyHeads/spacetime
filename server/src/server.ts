1a577caa736714d17cd304aa36808dcc686c3de4
Bruno <skyheads@live.com>
2024-02-17 21:51:18 -0300

    feat: import & register @fastify/jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
 import 'dotenv/config'
 
 import { authRoutes } from './routes/auth'
@@ -9,6 +11,9 @@ const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+app.register(jwt, {
+  secret: 'spacetime',
 })
 app.register(memoriesRoutes)
 app.register(authRoutes)
