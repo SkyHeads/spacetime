49bfe34ca6e839bf7fb5dce345fc83b0bf2ea3eb
Bruno <skyheads@live.com>
2024-03-09 02:33:54 -0300

    refactor: change domains to remotePatterns

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   domains: ['avatars.githubusercontent.com'],
+  // },
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
   },
 }
 
