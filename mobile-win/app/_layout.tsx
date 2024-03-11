916dab7ee3babeed0fef6b49c6064f6f36dafea8
Bruno <skyheads@live.com>
2024-03-11 03:07:37 -0300

    feat: useState for setIsUserAuthenticated & memories/new stack router

--- mobile-win/app/_layout.tsx
+++ mobile-win/app/_layout.tsx
@@ -2,6 +2,7 @@ import { styled } from 'nativewind'
 import { ImageBackground } from 'react-native'
 import { Slot, SplashScreen, Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
 
 import {
   useFonts,
@@ -12,38 +13,51 @@ import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
+import { useEffect, useState } from 'react'
 
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
-	const [hasLoadedFonts] = useFonts({
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<null | boolean>(null)
+
+  const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
 
-	if (!hasLoadedFonts) {
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
+
+  if (!hasLoadedFonts) {
     return <SplashScreen />
   }
 
-	return (
+  return (
     <>
-		<ImageBackground
-			source={blurBg}
-      className="relative flex-1 bg-gray-900"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-		>
-			<StyledStripes className="absolute left-2" />
-			<StatusBar style="light" translucent />
+      <ImageBackground
+        source={blurBg}
+        className="relative flex-1 bg-gray-900"
+        imageStyle={{ position: 'absolute', left: '-100%' }}
+      >
+        <StyledStripes className="absolute left-2" />
+        <StatusBar style="light" translucent />
 
-      {/* <Slot /> */}
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: 'transparent' },
-     }}
-    ></Stack>
-		</ImageBackground>
+        {/* <Slot /> */}
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: 'transparent' },
+          }}
+        >
+          <Stack.Screen name='index' redirect={isUserAuthenticated} />
+          <Stack.Screen name='new' />
+          <Stack.Screen name='memories' />
+        </Stack>
+      </ImageBackground>
     </>
-	)
+  )
 }
