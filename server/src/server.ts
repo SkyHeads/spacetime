4bd9db7dffd5bd00ed5a4af96a207190dc4e8c40
Bruno <skyheads@live.com>
2024-03-14 04:37:27 -0300

    feat: upload routes & register some used things

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,23 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
-
-import 'dotenv/config'
+import multipart from '@fastify/multipart'
+import staticFastify from '@fastify/static'
+import { resolve } from 'path'
 
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+app.register(staticFastify, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -15,8 +25,10 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'spacetime',
 })
+
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
