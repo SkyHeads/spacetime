9b401b2ac2ac097fdc8019fd0cfa21ebfa7c0316
Bruno <skyheads@live.com>
2024-04-04 02:37:51 -0300

    feat: open image picker preview image

--- mobile-win/app/new.tsx
+++ mobile-win/app/new.tsx
@@ -1,6 +1,7 @@
-import { View, Text, TouchableOpacity, Switch, ScrollView, TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, Switch, ScrollView, TextInput, Image } from 'react-native'
 import React, { useState } from 'react'
 import Icon from '@expo/vector-icons/Feather'
+import * as ImagePicker from 'expo-image-picker'
 
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -8,7 +9,29 @@ import { Link } from 'expo-router'
 
 export default function NewMemory() {
   const [isPublic, setIsPublic] = useState(false)
+  const [content, setContent] = useState('')
+  const [preview, setPreview] = useState<string | null>(null)
   const { bottom, top } = useSafeAreaInsets()
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (err) {
+      // deu erro mas nao tratei
+    }
+  }
+
+  function handleCreateMemory() {
+    console.log(content, isPublic)
+  }
 
   return (
     <ScrollView className='flex-1 px-8' contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}>
@@ -34,20 +57,47 @@ export default function NewMemory() {
           </Text>
         </View>
       </View>
+
       <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={openImagePicker}
         className='h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20'
       >
-        <View className='flex-row items-center gap-2'>
-          <Icon name='image' color='#fff' />
-          <Text className='text-sm font-bold text-gray-200'>Adicionar foto ou vídeo de capa</Text>
-        </View>
+        {preview ? (
+          <Image
+            source={{ uri: preview }}
+            className='h-full w-full rounded-lg object-cover'
+          />
+        ) : (
+          <View className='flex-row items-center gap-2'>
+            <Icon name='image' color='#fff' />
+            <Text className='text-sm font-bold text-gray-200'>
+              Adicionar foto ou vídeo de capa
+            </Text>
+          </View>
+        )}
       </TouchableOpacity>
+
       <TextInput
         multiline
+        value={content}
+        onChangeText={setContent}
+        textAlignVertical='top'
         className='p-0 font-body text-lg text-gray-50'
         placeholderTextColor="#56565a"
         placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que voce quer lembrar para sempre"
       />
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className='items-center self-end rounded-full bg-green-500 px-5 py-2'
+        onPress={handleCreateMemory}
+      >
+        <Text
+          className='font-alt text-sm uppercase text-black'
+        >
+          Salvar
+        </Text>
+      </TouchableOpacity>
     </ScrollView>
   )
 }
