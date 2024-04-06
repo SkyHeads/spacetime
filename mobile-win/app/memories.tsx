ae55b4cc79466309ebc70e01f5ecae59992937b8
Bruno <skyheads@live.com>
2024-04-06 17:46:58 -0300

    feat: memories list

--- mobile-win/app/memories.tsx
+++ mobile-win/app/memories.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'expo-router'
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Icon from '@expo/vector-icons/Feather'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
@@ -34,6 +34,75 @@ export default function Memories() {
               <Icon name="plus" size={16} color="#000" />
             </TouchableOpacity>
           </Link>
+        </View>
+      </View>
+
+      <View className='mt-6 space-y-10'>
+        <View className='space-y-4'>
+          <View className='flex-row items-center gap-2'>
+            <View className='h-px w-5 bg-gray-50' />
+            <Text className='font-body text-sm text-gray-100'>12 de Abril. 2024</Text>
+          </View>
+          <View className='space-y-4 px-8'>
+            <Image
+              source={{ uri: 'http://10.0.2.2:3333/uploads/f884df70-2986-4acc-bcc0-98560ca75804.jpg' }}
+              className='aspect-video w-full rounded-lg'
+              alt=''
+            />
+            <Text className='font-body text-base leading-relaxed text-gray-100'>
+              Lorem ipsom
+            </Text>
+            <Link href='/memories/id' asChild>
+              <TouchableOpacity className='flex-row items-center gap-2'>
+                <Text className='font-body text-sm text-gray-200'>Ler mais</Text>
+                <Icon name='arrow-right' size={16} color="#9e9ea0" />
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
+        <View className='space-y-4'>
+          <View className='flex-row items-center gap-2'>
+            <View className='h-px w-5 bg-gray-50' />
+            <Text className='font-body text-sm text-gray-100'>12 de Abril. 2024</Text>
+          </View>
+          <View className='space-y-4 px-8'>
+            <Image
+              source={{ uri: 'http://10.0.2.2:3333/uploads/f884df70-2986-4acc-bcc0-98560ca75804.jpg' }}
+              className='aspect-video w-full rounded-lg'
+              alt=''
+            />
+            <Text className='font-body text-base leading-relaxed text-gray-100'>
+              Lorem ipsom
+            </Text>
+            <Link href='/memories/id' asChild>
+              <TouchableOpacity className='flex-row items-center gap-2'>
+                <Text className='font-body text-sm text-gray-200'>Ler mais</Text>
+                <Icon name='arrow-right' size={16} color="#9e9ea0" />
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
+        <View className='space-y-4'>
+          <View className='flex-row items-center gap-2'>
+            <View className='h-px w-5 bg-gray-50' />
+            <Text className='font-body text-sm text-gray-100'>12 de Abril. 2024</Text>
+          </View>
+          <View className='space-y-4 px-8'>
+            <Image
+              source={{ uri: 'http://10.0.2.2:3333/uploads/f884df70-2986-4acc-bcc0-98560ca75804.jpg' }}
+              className='aspect-video w-full rounded-lg'
+              alt=''
+            />
+            <Text className='font-body text-base leading-relaxed text-gray-100'>
+              Lorem ipsom
+            </Text>
+            <Link href='/memories/id' asChild>
+              <TouchableOpacity className='flex-row items-center gap-2'>
+                <Text className='font-body text-sm text-gray-200'>Ler mais</Text>
+                <Icon name='arrow-right' size={16} color="#9e9ea0" />
+              </TouchableOpacity>
+            </Link>
+          </View>
         </View>
       </View>
     </ScrollView>
