dece4765d709a1d59cb0ba35d36d94d25ad0632c
Bruno <skyheads@live.com>
2024-04-16 02:21:13 -0300

    feat: load memories & dayjs for memory createdAt

--- mobile-win/app/memories.tsx
+++ mobile-win/app/memories.tsx
@@ -5,8 +5,23 @@ import Icon from '@expo/vector-icons/Feather'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import * as SecureStore from 'expo-secure-store'
 import { useRouter } from 'expo-router'
+import { api } from '../src/lib/api'
+import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-br'
+
+dayjs.locale(ptBR)
+
+interface Memory {
+  id: string
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+}
 
 export default function Memories() {
+  const [memories, setMemories] = useState<Memory[]>([])
+
   const { bottom, top } = useSafeAreaInsets()
   const router = useRouter()
 
@@ -16,6 +31,21 @@ export default function Memories() {
 
     router.push('/')
   }
+
+  async function loadMemories() {
+    const token = await SecureStore.getItemAsync('token')
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    setMemories(response.data)
+  }
+
+  useEffect(() => {
+    loadMemories()
+  }, [memories])
 
   return (
     <ScrollView className='flex-1 px-8' contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}>
@@ -38,72 +68,38 @@ export default function Memories() {
       </View>
 
       <View className='mt-6 space-y-10'>
-        <View className='space-y-4'>
-          <View className='flex-row items-center gap-2'>
-            <View className='h-px w-5 bg-gray-50' />
-            <Text className='font-body text-sm text-gray-100'>12 de Abril. 2024</Text>
-          </View>
-          <View className='space-y-4 px-8'>
-            <Image
-              source={{ uri: 'http://10.0.2.2:3333/uploads/f884df70-2986-4acc-bcc0-98560ca75804.jpg' }}
-              className='aspect-video w-full rounded-lg'
-              alt=''
-            />
-            <Text className='font-body text-base leading-relaxed text-gray-100'>
-              Lorem ipsom
-            </Text>
-            <Link href='/memories/id' asChild>
-              <TouchableOpacity className='flex-row items-center gap-2'>
-                <Text className='font-body text-sm text-gray-200'>Ler mais</Text>
-                <Icon name='arrow-right' size={16} color="#9e9ea0" />
-              </TouchableOpacity>
-            </Link>
-          </View>
-        </View>
-        <View className='space-y-4'>
-          <View className='flex-row items-center gap-2'>
-            <View className='h-px w-5 bg-gray-50' />
-            <Text className='font-body text-sm text-gray-100'>12 de Abril. 2024</Text>
-          </View>
-          <View className='space-y-4 px-8'>
-            <Image
-              source={{ uri: 'http://10.0.2.2:3333/uploads/f884df70-2986-4acc-bcc0-98560ca75804.jpg' }}
-              className='aspect-video w-full rounded-lg'
-              alt=''
-            />
-            <Text className='font-body text-base leading-relaxed text-gray-100'>
-              Lorem ipsom
-            </Text>
-            <Link href='/memories/id' asChild>
-              <TouchableOpacity className='flex-row items-center gap-2'>
-                <Text className='font-body text-sm text-gray-200'>Ler mais</Text>
-                <Icon name='arrow-right' size={16} color="#9e9ea0" />
-              </TouchableOpacity>
-            </Link>
-          </View>
-        </View>
-        <View className='space-y-4'>
-          <View className='flex-row items-center gap-2'>
-            <View className='h-px w-5 bg-gray-50' />
-            <Text className='font-body text-sm text-gray-100'>12 de Abril. 2024</Text>
-          </View>
-          <View className='space-y-4 px-8'>
-            <Image
-              source={{ uri: 'http://10.0.2.2:3333/uploads/f884df70-2986-4acc-bcc0-98560ca75804.jpg' }}
-              className='aspect-video w-full rounded-lg'
-              alt=''
-            />
-            <Text className='font-body text-base leading-relaxed text-gray-100'>
-              Lorem ipsom
-            </Text>
-            <Link href='/memories/id' asChild>
-              <TouchableOpacity className='flex-row items-center gap-2'>
-                <Text className='font-body text-sm text-gray-200'>Ler mais</Text>
-                <Icon name='arrow-right' size={16} color="#9e9ea0" />
-              </TouchableOpacity>
-            </Link>
-          </View>
-        </View>
+        {memories.map((memory) => {
+          return (
+            <View key={memory.id} className='space-y-4'>
+              <View className='flex-row items-center gap-2'>
+                <View className='h-px w-5 bg-gray-50' />
+                <Text className='font-body text-sm text-gray-100'>
+                  {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+                </Text>
+              </View>
+              <View className='space-y-4 px-8'>
+                <Image
+                  source={{
+                    uri: memory.coverUrl
+                  }}
+                  className='aspect-video w-full rounded-lg'
+                  alt=''
+                />
+                <Text className='font-body text-base leading-relaxed text-gray-100'>
+                  {memory.excerpt}
+                </Text>
+                <Link href='/memories/id' asChild>
+                  <TouchableOpacity className='flex-row items-center gap-2'>
+                    <Text className='font-body text-sm text-gray-200'>
+                      Ler mais
+                    </Text>
+                    <Icon name='arrow-right' size={16} color="#9e9ea0" />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          )
+        })}
       </View>
     </ScrollView>
   )
